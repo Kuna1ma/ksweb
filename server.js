2cb98f36aceaad24a41d65cb7a28b0eabd3289a1
Rexavier Cenon <144208721+Kuna1ma@users.noreply.github.com>
2025-10-05 00:10:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(cors());
 app.get("/config", (req, res) => {
   
   res.json({
-    maintenance: false, // toggle true/false
-    message: "Maintenance",
+    maintenance: true, // toggle true/false
+    message: "Pay $500 to resume",
     contact: "gaculacenon@icloud.com",
     lastUpdated: new Date().toISOString()
   });
